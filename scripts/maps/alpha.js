fd3c0f3ddb89b8e585ffc40430587c7fa7775305
Debojyoti Ghosh <debojyoti.ghosh@proptiger.com>
2020-01-16 01:42:52 +0530

    Dummy map files.

---
+++ scripts/maps/alpha.js
@@ -0,0 +1,22 @@
+{
+  "version": 1,
+  "config": {
+    "width": 1024,
+    "height": 640,
+    "tileWidth": 32,
+    "tileHeight": 32
+  },
+  "tiles": {
+    "groundL": [
+      { "x": 0, "y": 512 }
+    ]
+    "groundM": [
+      { "x": 128, "y": 512 },
+      { "x": 256, "y": 512 },
+      { "x": 384, "y": 512 }
+    ]
+    "groundR": [
+      { "x": 512, "y": 512 }
+    ]
+  }
+}
---
+++ scripts/maps/tiles.js
@@ -0,0 +1,9 @@
+{
+  "groundM": {
+    "name": "Ground middle",
+    "url": "/assets/images/ground.png",
+    "previewUrl": "/assets/images/ground.png",
+    "type": "ground",
+    "version": 1
+  }
+}
